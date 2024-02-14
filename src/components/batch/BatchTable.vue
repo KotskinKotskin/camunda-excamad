2a0c88b8d9d5f356e5ede005c02ad4e2c8fc0f5c
Vyacheslav Melnik <vyacheslav.melnik@dif.tech>
2024-02-14 11:21:56 +0400

    implement suspend all and delete all batches

--- src/components/batch/BatchTable.vue
+++ src/components/batch/BatchTable.vue
@@ -1,5 +1,14 @@
 <template>
 <div id="BatchTable">
+    <div v-if="loading" style="text-align: center; margin-bottom: 10px">
+      Sending {{requestCounter}}/{{batches.length}}
+    </div>
+
+    <div v-else class="main-buttons">
+      <b-button variant="outline-info" size="sm" @click="suspendAllBatches()" class="mr-2">Suspend All</b-button>
+      <b-button variant="outline-danger" size="sm" @click="deleteAllBatches()" class="mr-2">Delete All</b-button>
+    </div>
+
     <b-table :fields="fields" small bordered striped :items="batches" caption-top>
         <template v-slot:cell(delete)="data">
             <b-button variant="outline-danger" size="sm" @click="deleteBatch(data.item.id)" class="mr-2"> Delete</b-button>
@@ -17,6 +26,8 @@ export default {
     name: "BatchTable",
     data() {
         return {
+            loading: false,
+            requestCounter: 0,
             batches: [],
             fields: ["id", {
                     key: "type",
@@ -50,7 +61,6 @@ export default {
                     key: "delete",
                     label: "Delete"
                 },
-
             ]
         }
     },
@@ -58,13 +68,107 @@ export default {
         this.getBatches();
     },
     methods: {
+        deleteAllBatches() {
+          if (!window.confirm("Do you really want to delete all branches?")) {
+            return;
+          }
+
+          this.loading = true;
+          this.requestCounter = 0;
+          const total = this.batches.length;
+
+          this.$notify({
+            group: "foo",
+            title: "Deleting batches...",
+            type: "info"
+          });
+
+          Promise.allSettled(this.batches.map(batch => {
+            return this.$api().delete('/batch/' + batch.id + "?cascade=true").then(() => {
+              this.requestCounter++;
+            });
+          })).then((response) => {
+            const failed = response.filter(r => r.status === "rejected").length;
+            if (failed > 0) {
+              this.$notify({
+                group: "foo",
+                title: "Failed to delete batches",
+                text: `${failed} out of ${total} failed.`,
+                type: "warn",
+                duration: 5000
+              });
+            } else {
+              this.$notify({
+                group: "foo",
+                title: "All batches deleted!",
+                type: "success",
+                duration: 5000
+              });
+            }
+            this.getBatches();
+          }).catch(error => {
+            this.$notify({
+              group: "foo",
+              title: "Unexpected error occurred while deleting all batches",
+              text: error.message,
+              type: "error"
+            });
+          }).finally(() => this.loading = false);
+        },
+        suspendAllBatches() {
+          if (!window.confirm("Do you really want to suspend all branches?")) {
+            return;
+          }
+
+          this.requestCounter = 0;
+          this.loading = true;
+          const unsuspendedBatches = this.batches.filter(batch => !batch.suspended);
+          const total = unsuspendedBatches.length;
+
+          this.$notify({
+            group: "foo",
+            title: "Suspending batches...",
+            type: "info"
+          });
+
+          Promise.allSettled(unsuspendedBatches.map(batch => {
+            return this.$api().put('/batch/' + batch.id + "/suspended", {suspended: true}).then(() => {
+              this.requestCounter++;
+            });
+          })).then((response) => {
+            const failed = response.filter(r => r.status === "rejected").length;
+            if (failed > 0) {
+              this.$notify({
+                group: "foo",
+                title: "Failed to suspend batches",
+                text: `${failed} out of ${total} failed.`,
+                type: "warn",
+                duration: 5000
+              });
+            } else {
+              this.$notify({
+                group: "foo",
+                title: "All batches suspended!",
+                type: "success",
+                duration: 5000
+              });
+            }
+            this.getBatches();
+          }).catch(error => {
+            this.$notify({
+              group: "foo",
+              title: "Unexpected error occurred while suspending all batches",
+              text: error.message,
+              type: "error"
+            });
+          }).finally(() => this.loading = false);
+        },
         getBatches() {
             this.$api().get('/batch/statistics').then(response => {
                 this.batches = response.data;
             })
         },
         deleteBatch(batchId) {
-
             this.$notify({
                 group: "foo",
                 title: "Started, wait..",
@@ -124,4 +228,10 @@ export default {
 </script>
 
 <style>
+.main-buttons {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 10px;
+  gap: 10px;
+}
 </style>
